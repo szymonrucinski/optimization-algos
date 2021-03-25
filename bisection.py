d2a99fb44866e1e0f56c4bd0ff0fb4941a45a09a
szymonindy <szymonrucinski@icloud.com>
2021-03-25 19:23:11 +0100

    FIXED

--- bisection.py
+++ bisection.py
@@ -1,12 +1,13 @@
 from Equation import Expression
 import matplotlib.pyplot as plt
 import numpy as np
+import math
 
 
 def step_one(a, b):
-    l = a - b
-    x_1 = (a + l)/4.0
-    x_2 = (b - l)/4.0
+    l = b-a
+    x_1 = a + l/4.0
+    x_2 = b - l/4.0
     x_0 = (a + b)/2.0
     return x_1, x_2, x_0
 
@@ -20,9 +21,9 @@ def bisec(fun,a,b,epsilon,it_limit):
         if abs(b - a) <= 2*epsilon:
             break
 
-        elif fun(x_1) < fun(x_2):
+        elif fun(x_2) > fun(x_0) > fun(x_1):
             b = x_0
-        elif fun(x_2) < fun(x_0):
+        elif fun(x_2) < fun(x_0) < fun(x_1):
             a = x_0
         else:
             a = x_1
@@ -34,11 +35,10 @@ def draw_bisection(fun, a, b, eps, it_limit):
 
     x = np.linspace(a,b,200)
     y = fun(x)
-    print(fun(x), print(y))
 
     x_min = bisec(fun, a, b, eps, it_limit)
-    print(type(x_min))
     y_min = fun(x_min)
+    print('local:',x_min)
 
     # plotting the points
     plt.plot(x_min,y_min,'ro')  
@@ -52,4 +52,6 @@ def draw_bisection(fun, a, b, eps, it_limit):
 
 
 
-# draw_bisection(Expression("x^2 +2",["x"]), -6, 10, 0.1, 20)
+def fun(x):
+    return math.log(x)
+draw_bisection(Expression("(1/2)*(4-2*x^2)*x^2",["x"]), -2, 2, 0.1, 40)
