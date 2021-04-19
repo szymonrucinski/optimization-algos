3f2a4eb002d68915da0cdb3461954314d7daf5f0
Szymon <szymonrucinski@icloud.com>
2021-04-19 15:29:05 +0200

    updated algo

--- Task2/dfp.py
+++ Task2/dfp.py
@@ -39,20 +39,22 @@ def calculate_N(current_hessian_approx, gradient_diff):
     return N
 
 #Gradient approximation
-def update_hessian_approx(current_hessian_approx,S,next_gradient, current_gradient ,step):
-    #Secant equation
+def update_hessian_approx(current_hessian_approx,S,next_gradient, current_gradient ,step,delta_x):
+    # #Secant equation
 
-    B = current_hessian_approx
-    A_1 = np.dot(x_delta,x_delta.transpose())
+    # B = current_hessian_approx
+    # A_1 = np.dot(delta_x,delta_x.transpose())
+    # # A_2  =np.dot()
 
 
 
-    gradient_diff = np.subtract(next_gradient,current_gradient)
-    M = calculate_M(S,step, gradient_diff)
-    N = calculate_N(current_hessian_approx, gradient_diff)
+    # gradient_diff = np.subtract(next_gradient,current_gradient)
+    # M = calculate_M(S,step, gradient_diff)
+    # N = calculate_N(current_hessian_approx, gradient_diff)
 
-    current_hessian_approx = current_hessian_approx + M + N
-    return current_hessian_approx
+    # current_hessian_approx = current_hessian_approx + M + N
+    # return current_hessian_approx
+    return current_hessian_approx + 
   
 
 def new_update():
@@ -62,28 +64,51 @@ def new_update():
     v = np.divide()
 
 
+# def find_direction(hessian_approx, grad_m1, grad, step):
+#     H = hessian_approx
+#     grad_diff = grad - grad_m1
+#     ys = np.inner(grad_diff, step)
+#     Hy = np.dot(H, grad_diff)
+#     yHy = np.inner(grad_diff, Hy)
+#     H += (ys + yHy) * np.outer(step, step) / ys ** 2
+#     H -= (np.outer(Hy, step) + np.outer(step, Hy)) / ys
+#     direction = -np.dot(H, grad)
+#     return direction, {'gradient_diff': grad_diff}
 
-def fpd(x,y,it_num,step):
+
+
+def stop_fpd(gradient,epsilon):
+    print("stop hatl")
+    print(gradient)
+    result =  np.absolute(gradient) < epsilon
+    return np.any(result, where=[[False], [True]])
+
+def fpd(x,y,it_num,step,lambada):
     #Tego na razie nie wykorzystujemy
     epsilon = 0.5
     alpha = fun([x,y])
-    print('alpha', alpha)
+    lambada
 
- 
     #Macierz 2x2 o dodatnim wyznaczniku lub jednostkowa
     hessian_approx = np.identity(2)
-    point = np.array([[x,y]]).transpose()
+    point = np.array([[x,y]],np.float64).transpose()
 
     for i in range(0,it_num):
         current_gradient = gradient(point)
-        print(current_gradient)
         #1) Compute approx. Newton , update direction
+        # KIERUNEK POPRAWY
         S = -1 * np.dot(hessian_approx, current_gradient)
         #2) Line search (inexact)
-        point = point + np.dot(step,S)
+        delta_x = point
+        print("delta",delta_x)
+        point = point - np.dot(lambada,S)
+        print("delta after",delta_x)
+        delta_x = delta_x - point
         #3) Computer gradient for point x_(i+1)
         next_gradient = gradient(point)
         #4) Update Hessian
-        hessian_approx = update_hessian_approx(hessian_approx, S, next_gradient,current_gradient, step)
+        hessian_approx = update_hessian_approx(hessian_approx, S, next_gradient,current_gradient, step, delta_x)
+        if stop_fpd(current_gradient, epsilon):
+            break
 
-fpd(x = -2, y = -2 ,it_num = 3, step = 1)
+fpd(x = -2, y = -2 ,it_num = 3, step = 1, lambada = 0.2)
